05c46e83a78eb930d252fc9c7102842629890e7f
Jacob Carlborg <doob@me.com>
2018-05-09 16:23:28 +0200

    Allow to use this TextMate bundle with any 2.x.y version of Ruby
    If the `ruby20` is specified to run the commands in this bundle and
    the machine running TextMate has Ruby 2.x.y installed as the system
    Ruby, the system Ruby will be used. This basically means that any Ruby
    2.x.y version is allowed. Gems are installed in a directory with the
    same name as the Ruby ABI version, since this can change for minor
    version. Because of that, we need to use a custom location for the
    gems that works regardless of the Ruby ABI version. This should work
    fine since no C extensions are used by any of the gems used by this
    bundle.

--- Support/lib/gitlab_tm_bundle.rb
+++ Support/lib/gitlab_tm_bundle.rb
@@ -5,6 +5,33 @@ end
 lib = File.expand_path('..', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
+module Bundler
+  class RubygemsIntegration
+    # The reason this technique is used to override the `config_map` method is
+    # because if we define the `config_map` map method before `bundler/setup` is
+    # required, Bundler will override our version of the method and used the
+    # original one. If we try to override the `config_map` method after
+    # `bundler/setup` is required it will be too late and Bundler has already
+    # raised an error due to it not being able to find the gems.
+    def self.method_added(name)
+      return unless name == :config_map
+      return if @config_map_defined # Avoid infinite recursion
+
+      @config_map_defined = true
+
+      # Override the `config_map` method that will be later defined by Bundler.
+      # We do that to be able to make a copy of the `Gem::ConfigMap` constant
+      # which contains the Ruby ABI version. Then we change the ABI version to
+      # always be the same regardless of which version of Ruby is running this
+      # TextMate bundle. We make the copy to make sure only Bundler sees this
+      # change of the Ruby ABI version.
+      define_method :config_map do
+        @config_map ||= Gem::ConfigMap.dup.tap { |o| o[:ruby_version] = '2' }
+      end
+    end
+  end
+end
+
 require 'rubygems'
 require 'bundler/setup'
 
